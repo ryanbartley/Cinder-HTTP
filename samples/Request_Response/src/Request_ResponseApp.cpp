39204b6aac4accf97bcf89bde38007fa964dac02
Ryan Bartley <ryanbartley@gmail.com>
2016-08-12 23:35:39 -0400

    Added openssl and updated the sample

---
+++ samples/Request_Response/src/Request_ResponseApp.cpp
@@ -0,0 +1,84 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+#include "cinder/http/http.hpp"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class TestApp : public App {
+public:
+	void setup() override;
+	void mouseDown( MouseEvent event ) override;
+	void update() override;
+	void draw() override;
+	
+	void makeRequest( std::shared_ptr<http::url> url );
+	
+	std::shared_ptr<ci::http::Session>		session;
+	std::shared_ptr<ci::http::SslSession>	sslSession;
+	ci::gl::TextureRef texture;
+	std::shared_ptr<ci::http::url> httpUrl, httpsUrl;
+	bool useHttp = false;
+};
+
+void TestApp::setup()
+{
+	httpUrl = std::make_shared<http::url>( "http://cdn.tutsplus.com/net/authors/jeremymcpeak/http2-http.jpg" );
+	httpsUrl = std::make_shared<http::url>( "https://upload.wikimedia.org/wikipedia/commons/d/da/Internet2.jpg" );
+	
+	makeRequest( httpUrl );
+}
+
+void TestApp::makeRequest( std::shared_ptr<http::url> url )
+{
+	auto request = std::make_shared<http::Request>( http::RequestMethod::GET, url );
+	request->appendHeader( http::Connection( http::Connection::Type::CLOSE ) );
+	request->appendHeader( http::Accept() );
+	
+	auto onComplete = [&]( asio::error_code ec, http::Response response ) {
+		texture = ci::gl::Texture::create( loadImage( ci::DataSourceBuffer::create( response.getContent() ),
+													 ImageSource::Options(), ".jpg" ) );
+	};
+	auto onError = []( asio::error_code ec, http::Process process, const std::shared_ptr<http::url> &url, http::Response response ){
+		CI_LOG_E( ec.message() << " val: " << ec.value() << " Process: " << static_cast<uint32_t>( process )
+				 << " Url: " << url->to_string() );
+		std::cout << "Headers: " << std::endl;
+		std::cout << response.getHeaders() << endl;
+	};
+	
+	if( url->port() == 80 ) {
+		session = std::make_shared<http::Session>( url, request, onComplete, onError );
+		session->start();
+	}
+	else if( url->port() == 443 ) {
+		sslSession = std::make_shared<http::SslSession>( url, request, onComplete, onError );
+		sslSession->start();
+	}
+}
+
+void TestApp::mouseDown( MouseEvent event )
+{
+	if( useHttp )
+		makeRequest( httpUrl );
+	else
+		makeRequest( httpsUrl );
+	useHttp = ! useHttp;
+}
+
+void TestApp::update()
+{
+}
+
+void TestApp::draw()
+{
+	gl::clear( Color( 0, 0, 0 ) );
+	
+	gl::setMatricesWindow( getWindowSize() );
+	if( texture )
+		gl::draw( texture, texture->getBounds(), Rectf( 0, 0, getWindowWidth(), getWindowHeight() ) );
+}
+
+CINDER_APP( TestApp, RendererGl )
